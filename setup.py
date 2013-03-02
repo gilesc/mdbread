daa8fd873929b1ad4761a44bc0b8f86e80731321
Cory Giles <cory.b.giles@gmail.com>
2013-03-01 20:09:00 -0600

    Add informative error message if libmdb is not installed.

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
 import commands
+import sys
 
 def pkgconfig(*packages, **kw):
     flag_map = {'-I': 'include_dirs', 
                 '-L': 'library_dirs', 
                 '-l': 'libraries'}
-    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+    cmd = "pkg-config --libs --cflags %s" % ' '.join(packages)
+    status, out = commands.getstatusoutput(cmd)
+    if status != 0:
+        raise SystemExit("libmdb (or pkg-config) is not installed! Aborting...")
+    kw = {}
+    for token in out.split():
         kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
     return kw
 
